2763328911b9bcbeb85c5b9f286aa3f1415609be
okuda kohei <okuda@aska-ltd.jp>
2017-10-31 12:55:41 +0900

    テストコミット

--- base/core/Arr.php
+++ base/core/Arr.php
@@ -11,6 +11,7 @@ class Arr {
 
     public static $delimiter = '.';
 
+
     public static function get(array $array, $path, $default = null, $delimiter = null)
     {
         if (array_key_exists($path, $array))
